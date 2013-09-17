a18a0fd69cbbb6b2f42ba3ca1885144fc7906487
paulwittle <paulwittle@users.noreply.github.com>
2013-09-17 19:41:01 +0100

    Update mapconfiguration.js

--- php/apps/mapconfiguration.js
+++ php/apps/mapconfiguration.js
@@ -1,5 +1,5 @@
 var map, bounds, mapPanel, accordian, viewport, searchPanel;
-var osm, skiddlePoint, poiSaveStrategy;
+var osm, skiddlePoint, poiSaveStrategy, skiddleresult;
 Proj4js.defs["EPSG:27700"] = "+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000 +ellps=airy +datum=OSGB36 +units=m +no_defs";
 Proj4js.defs["EPSG:4326"] = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs";
 
@@ -157,7 +157,7 @@ function eventHandle(e){
 	lonlat = lonlat.transform(new OpenLayers.Projection("EPSG:900913"),new OpenLayers.Projection("EPSG:4326"));
 	
 	//Call the search
-	skiddlesearch(e.x,e.y,document.getElementById('buffersize').value);
+	skiddleresult = skiddlesearch(e.x,e.y,document.getElementById('buffersize').value);
 	
 	//Deactivate the tool
 	skiddlePoint.deactivate();
