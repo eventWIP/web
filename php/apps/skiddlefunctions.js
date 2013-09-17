1988bf935008dbdfa7e83ffe8af19ff974a48032
paulwittle <paulwittle@users.noreply.github.com>
2013-09-17 19:17:40 +0100

    Update skiddlefunctions.js

--- php/apps/skiddlefunctions.js
+++ php/apps/skiddlefunctions.js
@@ -7,8 +7,8 @@ function skiddlesearch(x,y,buffer){
 	var urlStr = "apps/skiddlerequest.php?eventcode=FEST&latitude="+y+"&longitude="+x+"&radius="+buffer;
 	$.get(urlStr,function(data){
 		if(data.responsetext!==null){
-			var returnArray = data;
-			alert(returnArray.toSource());
+			var returnArray = data.results;
+			alert(returnArray[0]);
 		} else {
 			alert("Sorry, your search did not return any results");
 		}
