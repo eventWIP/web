45850662b73153a36def803209110034a868d10d
paulwittle <paulwittle@users.noreply.github.com>
2013-09-18 12:55:24 +0100

    Update evt_popup.js

--- php/apps/evt_popup.js
+++ php/apps/evt_popup.js
@@ -1,15 +1,23 @@
 function evt_pop(e){
 	//OK first we need to find if there are any points near the location clicked
+	var obj_grab = {};
 	var obj_count = 0;
 	
 	//Did we find anything?
 	if (obj_count !== 0){
-		//Add the popup control
-		event_popup = new OpenLayers.LonLat(5,40),
-	               new OpenLayers.Size(200,200),
-	               "example popup",
-	               true);
-	
-	    	map.addPopup(popup);
+		var i = 0;
+		$.each(obj_grab,obj){
+			//Add the popup controls
+			var obj_name = "event_popup" + i;
+			var obj_html = ""
+			window[obj_name] = new OpenLayers.Popup("chicken",
+				obj.point,
+		               	new OpenLayers.Size(200,200),
+		               	obj_html,
+		               	true);
+		
+		    	map.addPopup(popup);
+		    	i = i + 1;
+		}
 	}
 }
