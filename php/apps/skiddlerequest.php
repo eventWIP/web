15f1650b62cdde5db2ac4f1a49681dc3d9b54bba
paulwittle <paulwittle@users.noreply.github.com>
2013-09-18 12:33:50 +0100

    Update skiddlerequest.php

--- php/apps/skiddlerequest.php
+++ php/apps/skiddlerequest.php
@@ -13,11 +13,11 @@
 	if ($eventcode===''){
 		//Create a URL
 		$url = "http://www.skiddle.com/api/v1/events/?api_key=" . $api_key . "&latitude=";
-		$url .= $latitude . "&longitude=" . $longitude . "&radius=" . $radius;	
+		$url .= $latitude . "&longitude=" . $longitude . "&radius=" . $radius . "&limit=100&offset=0";	
 	} else {
 		//Create a URL
 		$url = "http://www.skiddle.com/api/v1/events/?api_key=" . $api_key . "&eventcode=" . $eventcode . "&latitude=";
-		$url .= $latitude . "&longitude=" . $longitude . "&radius=" . $radius;
+		$url .= $latitude . "&longitude=" . $longitude . "&radius=" . $radius . "&limit=100&offset=0";
 	}
 	
 	//Obtain a response from the server
