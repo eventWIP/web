567aac7499a3f4b8426e9349822fbf86261271eb
matt20013 <matthew.hodgskiss@gmail.com>
2013-09-18 09:39:48 +0100

    Create metofficefunctions.js

---
+++ php/apps/metofficefunctions.js
@@ -0,0 +1,11 @@
+function metofficesearch(location_id){
+	var urlStr = "apps/metofficerequest.php?location_id="+location_id+";
+	$.get(urlStr,function(data){
+		if(data.responsetext!==null){
+			var returnArray = data
+			alert(returnArray[0]);
+		} else {
+			alert("Sorry, your search did not return any results");
+		}
+	});
+}
