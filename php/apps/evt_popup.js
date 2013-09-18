ca3ac75b1f9e90f1d42469b751a9c518ddbd6ff6
paulwittle <paulwittle@users.noreply.github.com>
2013-09-18 13:24:05 +0100

    Update evt_popup.js

--- php/apps/evt_popup.js
+++ php/apps/evt_popup.js
@@ -2,22 +2,37 @@ function evt_pop(e){
 	//OK first we need to find if there are any points near the location clicked
 	var obj_grab = {};
 	var obj_count = 0;
-	
-	//Did we find anything?
-	if (obj_count !== 0){
+	if (typeof e !== 'undefined'){
+		//We only process one result here
 		var i = 0;
-		$.each(obj_grab, function(obj){
-			//Add the popup controls
-			var obj_name = "event_popup" + i;
-			var obj_html = ""
-			window[obj_name] = new OpenLayers.Popup("chicken",
-				obj.point,
-		               	new OpenLayers.Size(200,200),
-		               	obj_html,
-		               	true);
+		var event_location = new OpenLayers.Geometry.Point(e.attributes.longitude, e.attributes.latitude);
+		var event_id = e.attributes.event_id;
 		
-		    	map.addPopup(popup);
-		    	i = i + 1;
-		});
+		//Add the popup controls
+		var obj_name = "event_popup" + i;
+		var obj_html = "<b>" + e.attributes.event_name + "</b><br />";
+		obj_html += "The event is taking place on " + e.attributes.event_date + " <br />";
+		obj_html += "This event is";
+		if (e.attributes.event_price==0){
+			obj_html += " free. <br />";
+		} else {
+			obj_html += " ticket event. Tickets cost £" + e.attributes.event_price + " <br />";
+		}
+		obj_html += "There are " + e.attributes.fb_attend + " attending & " + e.attributes.fb_maybe + " maybe according to Facebook <br />";
+		obj_html += "The event scored " + e.attributes.twit_score + " on our Twitter populatity test <br />";
+		//if (e.attributes.event_date within 5 days){
+		obj_html += "Based on the current weather forcast this event has been given a status of ";
+		//Status cases go here
+		obj_html += "<br />";
+		//Add the icon
+		obj_html += "<img src='img/' />";
+		
+		window[obj_name] = new OpenLayers.Popup("chicken",
+			event_location,
+	               	new OpenLayers.Size(200,200),
+	               	obj_html,
+	               	true);
+	
+	    	map.addPopup(window[obj_name]);
 	}
 }
