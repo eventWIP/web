1303d90363df349047b1e00e43b24574c1f28c48
matt20013 <matthew.hodgskiss@gmail.com>
2013-09-18 10:36:17 +0100

    Update metofficefunctions.js

--- php/apps/metofficefunctions.js
+++ php/apps/metofficefunctions.js
@@ -1,11 +1,29 @@
 function metofficesearch(location_id){
+
 	var urlStr = "apps/metofficerequest.php?location_id="+location_id;
-	$.get(urlStr,function(data){
-		if(data.responsetext!==null){
-			var returnArray = data
-			alert(returnArray[0]);
+	weather_info ={};
+	$.ajax({url:urlStr,async:false,success:function(data){
+		if(data !== 0){
+			var returnArray = data.SiteRep.DV.Location;
+
+			$.each(returnArray, function(key, weather_result) {
+				weather_obj = {
+					
+					
+					"weather_station_name":weather_result.name
+				
+				};
+
+				event_info[weather_result.i] = weather_obj;
+			});
+
+
+
 		} else {
 			alert("Sorry, your search did not return any results");
 		}
-	});
+	}});
+
+	return weather_info;
+	
 }
