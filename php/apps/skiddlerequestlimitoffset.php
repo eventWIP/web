87af474659beae3b9e2e112355b9235cd58bb453
matt20013 <matthew.hodgskiss@gmail.com>
2013-09-18 12:46:11 +0100

    Create skiddlerequestlimitoffset.php

---
+++ php/apps/skiddlerequestlimitoffset.php
@@ -0,0 +1,30 @@
+<?php
+	//The output will be json
+	header('Content-type: application/json');
+
+	//Pickup the variables
+	$api_key = "838cec44cf59198b22731f4be213988a";
+	$eventcode = $_GET['eventcode'];
+	$latitude = $_GET['latitude'];
+	$longitude = $_GET['longitude'];
+	$radius = $_GET['radius'];
+  $limit = $_GET['limit'];
+  $offset = $_GET['offset'];
+
+	//String validation
+	if ($eventcode===''){
+		//Create a URL
+		$url = "http://www.skiddle.com/api/v1/events/?api_key=" . $api_key . "&latitude=";
+		$url .= $latitude . "&longitude=" . $longitude . "&radius=" . $radius . "&limit=" . $limit . "&offset=" . $offset;	
+	} else {
+		//Create a URL
+		$url = "http://www.skiddle.com/api/v1/events/?api_key=" . $api_key . "&eventcode=" . $eventcode . "&latitude=";
+		$url .= $latitude . "&longitude=" . $longitude . "&radius=" . $radius . "&limit=" . $limit . "&offset=" . $offset;
+	}
+
+	//Obtain a response from the server
+	$response = file_get_contents($url);
+
+	//Write the contents of the file
+	print $response;
+?>
