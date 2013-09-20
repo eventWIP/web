8b842e30331ea2e0f978a444c79e8f2376a505f7
paulwittle <paulwittle@users.noreply.github.com>
2013-09-20 21:17:23 +0100

    Create fb_request.php

---
+++ php/apps/fb_request.php
@@ -0,0 +1,12 @@
+<?php
+
+$rtype = $_GET['rtype'];
+$type = $_GET['type'];
+$q = $_GET['q'];
+
+if($rtype==1){
+
+  $url = 'https://graph.facebook.com/search?q=' . $q . '&type=' . $type . '&access_token=617411168311328|	446eb7eebc20af4d5f9b780c883db247';
+}
+
+?>
