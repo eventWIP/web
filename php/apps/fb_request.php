729cce3f8f52f8b1e7c9ea2e85f65fdf75362df1
paulwittle <paulwittle@users.noreply.github.com>
2013-09-20 21:24:01 +0100

    Update fb_request.php

--- php/apps/fb_request.php
+++ php/apps/fb_request.php
@@ -3,10 +3,17 @@
 $rtype = $_GET['rtype'];
 $type = $_GET['type'];
 $q = $_GET['q'];
+$objID = $_GET['objID'];
 
 if($rtype==1){
 
   $url = 'https://graph.facebook.com/search?q=' . $q . '&type=' . $type . '&access_token=617411168311328|446eb7eebc20af4d5f9b780c883db247';
+} elseif ($rtype==2){
+  $url = 'https://graph.facebook.com/' . $objID . '?method=GET&format=json&suppress_http_code=1&access_token=617411168311328|446eb7eebc20af4d5f9b780c883db247';
+} elseif ($rtype==3){
+  $url = 'https://graph.facebook.com/' . $objID . '/attending?method=GET&format=json&suppress_http_code=1&access_token=617411168311328|446eb7eebc20af4d5f9b780c883db247';
+} elseif ($rtype==4){
+  $url = 'https://graph.facebook.com/' . $objID . '/maybe?method=GET&format=json&suppress_http_code=1&access_token=617411168311328|446eb7eebc20af4d5f9b780c883db247';
 }
 
 ?>
