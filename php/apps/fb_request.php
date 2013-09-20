5cf7b60e27aca845e05f7ce7681b48bb83f9ee55
paulwittle <paulwittle@users.noreply.github.com>
2013-09-20 21:17:45 +0100

    Update fb_request.php

--- php/apps/fb_request.php
+++ php/apps/fb_request.php
@@ -6,7 +6,7 @@ $q = $_GET['q'];
 
 if($rtype==1){
 
-  $url = 'https://graph.facebook.com/search?q=' . $q . '&type=' . $type . '&access_token=617411168311328|	446eb7eebc20af4d5f9b780c883db247';
+  $url = 'https://graph.facebook.com/search?q=' . $q . '&type=' . $type . '&access_token=617411168311328|446eb7eebc20af4d5f9b780c883db247';
 }
 
 ?>
