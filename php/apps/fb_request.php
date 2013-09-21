92e95db033eeff2619a240950006e873c2cd26c5
paulwittle <paulwittle@users.noreply.github.com>
2013-09-21 10:54:20 +0100

    Update fb_request.php

--- php/apps/fb_request.php
+++ php/apps/fb_request.php
@@ -11,13 +11,13 @@
   //$appsecret_proof = hash_hmac('sha256', '617411168311328', 'How do we hide it?');
   
   if($rtype==1){
-    $url = 'https://graph.facebook.com/search?q=' . $q . '&type=' . $type . '&access_token=617411168311328|1';
+    $url = 'https://graph.facebook.com/search?q=' . $q . '&type=' . $type . '&access_token=CAACEdEose0cBAAO10qnRkyuiFtwiz1fLP2X1g3pJiT0aCHpJMbwk9JKfCEviuhUA3q4PhA5ZA1AVU6eW9jsKvW2A58UQ8WTgQZCSuO4tx7mqz3anjgdKksd1ZAeFhrKAhgYOZADK3PwZAeAZCZB8pJkBAqfCvnUMmGfMK76rCxfKZCP2Fhfm05qn0pIZBAUMrsxMh5NxssQzd2QZDZD';
   } elseif ($rtype==2){
-    $url = 'https://graph.facebook.com/' . $objID . '?method=GET&format=json&suppress_http_code=1&access_token=617411168311328|1';
+    $url = 'https://graph.facebook.com/' . $objID . '?method=GET&format=json&suppress_http_code=1&access_token=CAACEdEose0cBAAO10qnRkyuiFtwiz1fLP2X1g3pJiT0aCHpJMbwk9JKfCEviuhUA3q4PhA5ZA1AVU6eW9jsKvW2A58UQ8WTgQZCSuO4tx7mqz3anjgdKksd1ZAeFhrKAhgYOZADK3PwZAeAZCZB8pJkBAqfCvnUMmGfMK76rCxfKZCP2Fhfm05qn0pIZBAUMrsxMh5NxssQzd2QZDZD';
   } elseif ($rtype==3){
-    $url = 'https://graph.facebook.com/' . $objID . '/attending?method=GET&format=json&suppress_http_code=1&access_token=617411168311328|1';
+    $url = 'https://graph.facebook.com/' . $objID . '/attending?method=GET&format=json&suppress_http_code=1&access_token=CAACEdEose0cBAAO10qnRkyuiFtwiz1fLP2X1g3pJiT0aCHpJMbwk9JKfCEviuhUA3q4PhA5ZA1AVU6eW9jsKvW2A58UQ8WTgQZCSuO4tx7mqz3anjgdKksd1ZAeFhrKAhgYOZADK3PwZAeAZCZB8pJkBAqfCvnUMmGfMK76rCxfKZCP2Fhfm05qn0pIZBAUMrsxMh5NxssQzd2QZDZD';
   } elseif ($rtype==4){
-    $url = 'https://graph.facebook.com/' . $objID . '/maybe?method=GET&format=json&suppress_http_code=1&access_token=617411168311328|1';
+    $url = 'https://graph.facebook.com/' . $objID . '/maybe?method=GET&format=json&suppress_http_code=1&access_token=CAACEdEose0cBAAO10qnRkyuiFtwiz1fLP2X1g3pJiT0aCHpJMbwk9JKfCEviuhUA3q4PhA5ZA1AVU6eW9jsKvW2A58UQ8WTgQZCSuO4tx7mqz3anjgdKksd1ZAeFhrKAhgYOZADK3PwZAeAZCZB8pJkBAqfCvnUMmGfMK76rCxfKZCP2Fhfm05qn0pIZBAUMrsxMh5NxssQzd2QZDZD';
   }
 
   //Obtain a response from the server
