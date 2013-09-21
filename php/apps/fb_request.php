4aaa0db4b1cb79ac0d3ce90d3baf577f9b2f8ae5
paulwittle <paulwittle@users.noreply.github.com>
2013-09-21 08:26:20 +0100

    Update fb_request.php

--- php/apps/fb_request.php
+++ php/apps/fb_request.php
@@ -12,11 +12,11 @@
   if($rtype==1){
     $url = 'https://graph.facebook.com/search?q=' . $q . '&type=' . $type . '&access_token=617411168311328|234098';
   } elseif ($rtype==2){
-    //$url = 'https://graph.facebook.com/' . $objID . '?method=GET&format=json&suppress_http_code=1&access_token=617411168311328|hidden';
+    $url = 'https://graph.facebook.com/' . $objID . '?method=GET&format=json&suppress_http_code=1&access_token=617411168311328|234098';
   } elseif ($rtype==3){
-    //$url = 'https://graph.facebook.com/' . $objID . '/attending?method=GET&format=json&suppress_http_code=1&access_token=617411168311328|hidden';
+    $url = 'https://graph.facebook.com/' . $objID . '/attending?method=GET&format=json&suppress_http_code=1&access_token=617411168311328|234098';
   } elseif ($rtype==4){
-    //$url = 'https://graph.facebook.com/' . $objID . '/maybe?method=GET&format=json&suppress_http_code=1&access_token=617411168311328|hidden';
+    $url = 'https://graph.facebook.com/' . $objID . '/maybe?method=GET&format=json&suppress_http_code=1&access_token=617411168311328|234098';
   }
 
 	//Obtain a response from the server
