f0576e946ef5509173e2297a24ca3d4a73686ba1
paulwittle <paulwittle@users.noreply.github.com>
2013-09-21 09:43:06 +0100

    Update fb_request.php

--- php/apps/fb_request.php
+++ php/apps/fb_request.php
@@ -8,16 +8,16 @@
   $objID = $_GET['objID'];
   
   //How do we hide the secret code?
-  $appsecret_proof = hash_hmac('sha256', '617411168311328', '446eb7eebc20af4d5f9b780c883db247');
+  //$appsecret_proof = hash_hmac('sha256', '617411168311328', 'How do we hide it?');
   
   if($rtype==1){
-    $url = 'https://graph.facebook.com/search?q=' . $q . '&type=' . $type . '&access_token=617411168311328|446eb7eebc20af4d5f9b780c883db247';
+    $url = 'https://graph.facebook.com/search?q=' . $q . '&type=' . $type . '&access_token=617411168311328|1';
   } elseif ($rtype==2){
-    $url = 'https://graph.facebook.com/' . $objID . '?method=GET&format=json&suppress_http_code=1&access_token=617411168311328|446eb7eebc20af4d5f9b780c883db247';
+    $url = 'https://graph.facebook.com/' . $objID . '?method=GET&format=json&suppress_http_code=1&access_token=617411168311328|1';
   } elseif ($rtype==3){
-    $url = 'https://graph.facebook.com/' . $objID . '/attending?method=GET&format=json&suppress_http_code=1&access_token=617411168311328|446eb7eebc20af4d5f9b780c883db247';
+    $url = 'https://graph.facebook.com/' . $objID . '/attending?method=GET&format=json&suppress_http_code=1&access_token=617411168311328|1';
   } elseif ($rtype==4){
-    $url = 'https://graph.facebook.com/' . $objID . '/maybe?method=GET&format=json&suppress_http_code=1&access_token=617411168311328|446eb7eebc20af4d5f9b780c883db247';
+    $url = 'https://graph.facebook.com/' . $objID . '/maybe?method=GET&format=json&suppress_http_code=1&access_token=617411168311328|1';
   }
 
   //Obtain a response from the server
